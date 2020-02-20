767248fc7e3857804f19675a9ad11b2049c6c18b
Ido <ido@ojos.jp>
2020-02-20 02:10:41 +0000

    added launch.json .gitattributes

--- app/core/urls.py
+++ app/core/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse('OK')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('health_check', health_check, name='health_check'),
+    path('__admin__/', admin.site.urls),
 ]
